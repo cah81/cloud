aa0e3a7ace81b95c252bff9f30a6b1b99c978d17
cah81 <carles1981@gmail.com>
2023-03-22 18:02:49 +0100

    Initial commit

---
+++ eureka-server/src/test/java/com/eureka/example/SpringCloudEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/main/java/com/car/CartRestConsumer.java
@@ -0,0 +1,42 @@
+package com.car;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Component
+public class CartRestConsumer {
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    public String serviceUrl(){
+        List<ServiceInstance> list = discoveryClient.getInstances("CART-SERVICE");
+        if(list !=null && list.size()>0){
+            ServiceInstance serviceInstance = list.get(0);
+            return serviceInstance.getUri()+ "/cart/getData";
+        }
+        return null;
+
+    }
+
+    public String getCartInfo(){
+        // get ServiceInstance list using serviceId
+        //List<ServiceInstance> list = discoveryClient.getInstances("CART-SERVICE");
+        // read manually one instance from index#0
+        //ServiceInstance serviceInstance = list.get(0);
+        // read URI and Add path that returns url
+        //String url = serviceInstance.getUri() + "cart/getData";
+        // create object for RestTemplate
+        RestTemplate rt = new RestTemplate();
+        // make HTTP call and get Reponse data
+        String response = rt.getForObject(serviceUrl(),String.class);
+        // return response back to Consumer
+        return response;
+    }
+
+}
